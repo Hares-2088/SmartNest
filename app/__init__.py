141d7efbcafd763b0fed11c1dad095d696b912b5
PortgasD.Adem <121312399+Hares-2088@users.noreply.github.com>
2024-12-16 01:11:41 -0500

    yes

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,7 @@
-# ...existing code...
-from app.models import setup_database, load_user
-# ...existing code...
+#This script is used to view the existing users in the database and add a default user if none exist.
+from .auth import auth_bp
+from .api import api_bp
+from .dashboard import dashboard_bp
+
+# List of all blueprints for easier registration
+blueprints = [auth_bp, api_bp, dashboard_bp]
